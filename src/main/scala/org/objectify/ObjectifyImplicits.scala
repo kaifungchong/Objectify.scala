24ddaa7ef54c713b86c46caa7dfb1bd90f07fa59
Arthur Gonigberg <artgon@gmail.com>
2012-07-12 12:13:24 -0700

    - added implicit

--- src/main/scala/org/objectify/ObjectifyImplicits.scala
+++ src/main/scala/org/objectify/ObjectifyImplicits.scala
@@ -8,4 +8,10 @@ import responders.PolicyResponder
   */
 trait ObjectifyImplicits {
     implicit def tuple2PolicyTuple(policy: (Class[_ <: Policy], Class[_ <: PolicyResponder[_]])) = new PolicyTuple(policy)
+
+    implicit def map2PolicyTupleSeq(policyMap: Map[Class[_ <: Policy], Class[_ <: PolicyResponder[_]]]) = {
+        policyMap.map {
+            case (pol, responder) => new PolicyTuple(pol, responder)
+        }(collection.breakOut): Seq[PolicyTuple]
+    }
 }
