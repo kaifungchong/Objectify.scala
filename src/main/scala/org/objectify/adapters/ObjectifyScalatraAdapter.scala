3837e8d25cf1519c4a306206397905b25488d75d
Arthur Gonigberg <artgon@gmail.com>
2013-08-06 13:04:24 -0700

    added license and copyright

--- src/main/scala/org/objectify/adapters/ObjectifyScalatraAdapter.scala
+++ src/main/scala/org/objectify/adapters/ObjectifyScalatraAdapter.scala
@@ -1,3 +1,12 @@
+/*
+ * -------------------------------------------------------------------------------------------------
+ *  - Project:   Objectify                                                                           -
+ *  - Copyright: ©2013 Matygo Educational Incorporated operating as Learndot                         -
+ *  - Author:    Arthur Gonigberg (arthur@learndot.com) and contributors (see contributors.txt)      -
+ *  - License:   Licensed under MIT license (see license.txt)                                         -
+ *  -------------------------------------------------------------------------------------------------
+ */
+
 package org.objectify.adapters
 
 import org.objectify.HttpMethod.Delete
