8902019b6deed99940b02eb44a5cc4f99aa32281
Joe Gaudet <joe@learndot.com>
2015-02-28 16:05:39 +0800

    Scala 2.11.4 support

--- src/main/scala/org/objectify/adapters/ObjectifyScalatraAdapter.scala
+++ src/main/scala/org/objectify/adapters/ObjectifyScalatraAdapter.scala
@@ -9,83 +9,79 @@
 
 package org.objectify.adapters
 
-import org.objectify.HttpMethod.Delete
-import org.objectify.HttpMethod.Get
-import org.objectify.HttpMethod.Options
-import org.objectify.HttpMethod.Patch
-import org.objectify.HttpMethod.Post
-import org.objectify.HttpMethod.Put
+import org.objectify.HttpMethod.{Delete, Get, Options, Patch, Post, Put}
 import org.objectify.Objectify
-import org.scalatra.servlet.{RichResponse, RichRequest, ServletBase}
-import org.objectify.exceptions.{ObjectifyExceptionWithCause, ObjectifyException, BadRequestException}
-import org.scalatra.fileupload.FileUploadSupport
+import org.objectify.exceptions.{BadRequestException, ObjectifyException, ObjectifyExceptionWithCause}
 import org.objectify.resolvers.ClassResolver
+import org.scalatra.fileupload.FileUploadSupport
+import org.scalatra.servlet.{RichRequest, RichResponse, ServletBase}
 
 trait ObjectifyScalatraAdapter extends Objectify with ServletBase with FileUploadSupport {
 
+  /**
+   * Decorates the default bootstrap which has the configuration
+   * validation in it
+   */
+  override def bootstrap() {
+    super.bootstrap()
+
     /**
-      * Decorates the default bootstrap which has the configuration
-      * validation in it
-      */
-    override def bootstrap() {
-        super.bootstrap()
-
-        /**
-          * Sort wildcards to the top so that there are no route conflicts and that routes are always
-          * added consistently.
-          */
-        val sortedActions = actions.toList.sortBy(_.route.getOrElse(""))(new Ordering[String] {
-            def compare(x: String, y: String) = {
-                val wildcardSymbol = ':'
-                if (x.contains(wildcardSymbol) && y.contains(wildcardSymbol)) {
-                    0
-                }
-                else if (x.contains(wildcardSymbol)) {
-                    -1
-                }
-                else if (y.contains(wildcardSymbol)) {
-                    1
-                }
-                else {
-                    0
-                }
-            }
-        })
-
-        /**
-          * For each action we map to the Scalatra equivalent block parameter
-          * though we are still bound to the HttpServletRequest and Response
-          * currently.
-          */
-        sortedActions.foreach(action => {
-            def scalatraFunction(route: String) = action.method match {
-                case Options => options(route) _
-                case Get => get(route) _
-                case Post => post(route) _
-                case Put => put(route) _
-                case Delete => delete(route) _
-                case Patch => patch(route) _
-            }
-
-            scalatraFunction("/" + action.route.getOrElse(throw new BadRequestException("No Route Found"))) {
-                // wrap HttpServletRequest in adapter and get ObjectifyResponse
-                val objectifyResponse = execute(action,
-                    new ScalatraRequestAdapter(RichRequest(request), RichResponse(response), params.toMap, Some(fileParams)))
-
-                // find appropriate response adapter and serialize the response
-                ClassResolver.locateResponseAdapter(objectifyResponse).serializeResponseAny(request, response, objectifyResponse)
-            }
-        })
-    }
-
-    error {
-        case e: ObjectifyException => {
-            status = e.status
-            e.getMessage
+     * Sort wildcards to the top so that there are no route conflicts and that routes are always
+     * added consistently.
+     */
+    val sortedActions = actions.toList.sortBy(_.route.getOrElse(""))(new Ordering[String] {
+      def compare(x: String, y: String) = {
+        val wildcardSymbol = ':'
+        if (x.indexOf(wildcardSymbol) != -1 && y.indexOf(wildcardSymbol) != -1) {
+          0
         }
-        case e: ObjectifyExceptionWithCause => {
-            status = e.status
-            e.getMessage
+        else if (x.indexOf(wildcardSymbol) != -1) {
+          -1
         }
+        else if (y.indexOf(wildcardSymbol) != -1) {
+          1
+        }
+        else {
+          0
+        }
+      }
+    })
+
+    /**
+     * For each action we map to the Scalatra equivalent block parameter
+     * though we are still bound to the HttpServletRequest and Response
+     * currently.
+     */
+    sortedActions.foreach(action => {
+      def scalatraFunction(route: String) = action.method match {
+        case Options => options(route) _
+        case Get => get(route) _
+        case Post => post(route) _
+        case Put => put(route) _
+        case Delete => delete(route) _
+        case Patch => patch(route) _
+      }
+
+      scalatraFunction("/" + action.route.getOrElse(throw new BadRequestException("No Route Found"))) {
+        // wrap HttpServletRequest in adapter and get ObjectifyResponse
+        val objectifyResponse = execute(action,
+          new ScalatraRequestAdapter(RichRequest(request), RichResponse(response), params.toMap, Some(fileParams)))
+
+        // find appropriate response adapter and serialize the response
+        ClassResolver.locateResponseAdapter(objectifyResponse).serializeResponseAny(request, response, objectifyResponse)
+      }
+    })
+  }
+
+  error {
+    case e: ObjectifyException => {
+      status = e.status
+      e.getMessage
     }
+    case e: ObjectifyExceptionWithCause => {
+      status = e.status
+      e.getMessage
+    }
+  }
 }
+
--- src/test/scala/org/objectify/adapters/ObjectifyScalatraAdapterTest.scala
+++ src/test/scala/org/objectify/adapters/ObjectifyScalatraAdapterTest.scala
@@ -24,192 +24,192 @@ import org.objectify.services.{ThrowsUnexpected, Throws403, ThrowsConfig, Throws
 
 
 /**
-  * Testing the Scalatra adapter
-  */
+ * Testing the Scalatra adapter
+ */
 @RunWith(classOf[JUnitRunner])
 class ObjectifyScalatraAdapterTest
-    extends WordSpec with BeforeAndAfterEach with MockitoSugar with ObjectifySugar with Matchers with ScalatraSuite {
+  extends WordSpec with BeforeAndAfterEach with MockitoSugar with ObjectifySugar with Matchers with ScalatraSuite {
 
-    val scalatrafied = new ObjectifyScalatraAdapter with ScalatraFilter {
+  val scalatrafied = new ObjectifyScalatraAdapter with ScalatraFilter {
+    get("/test") {
+      "win"
+    }
+  }
+
+  override def beforeEach() {
+    addFilter(scalatrafied, "/*")
+
+    scalatrafied.actions resource "pictures"
+    scalatrafied.bootstrap()
+  }
+
+  "The Scalatra adapter" when {
+
+    "doing happy path cases" should {
+
+      "do control" in {
         get("/test") {
-            "win"
+          status should equal(200)
+          body should include("win")
         }
+        get("/asdfqwerty") {
+          status should equal(404)
+        }
+      }
+      "get pictures index" in {
+        get("/pictures") {
+          status should equal(200)
+          body should include("index")
+        }
+      }
+      "get pictures show" in {
+        get("/pictures/12") {
+          status should equal(200)
+          body should include("show 12")
+        }
+      }
+      "post pictures create" in {
+        post("/pictures") {
+          status should equal(200)
+          body should include("create")
+        }
+      }
+      "put pictures update" in {
+        put("/pictures/12") {
+          status should equal(200)
+          body should include("update")
+        }
+      }
+      "delete pictures destroy" in {
+        delete("/pictures/12") {
+          status should equal(200)
+          body should include("destroy")
+        }
+      }
     }
 
-    override def beforeEach() {
-        addFilter(scalatrafied, "/*")
-
-        scalatrafied.actions resource "pictures"
+    "checking content type" should {
+      "return JSON by default" in {
+        get("/pictures") {
+          header("Content-Type") should include("application/json")
+        }
+      }
+      "return overridden type" in {
+        scalatrafied.actions resource("pictures", index = Some(Action(Get, "index", ContentType.XML)))
         scalatrafied.bootstrap()
+        get("/pictures") {
+          header("Content-Type") should include("application/xml")
+        }
+      }
     }
 
-    "The Scalatra adapter" when {
+    "doing exceptional cases" should {
+      "return a 400 code for bad request" in {
+        scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
+          policies = Some(Map(
+            ~:[GoodPolicy] -> ~:[BadPolicyResponder],
+            ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
+          ),
+          service = Some(~:[ThrowsBadRequest]),
+          responder = Some(~:[PicturesIndexResponder]))
+        ))
+        scalatrafied.bootstrap()
 
-        "doing happy path cases" should {
-
-            "do control" in {
-                get("/test") {
-                    status should equal(200)
-                    body should include("win")
-                }
-                get("/asdfqwerty") {
-                    status should equal(404)
-                }
-            }
-            "get pictures index" in {
-                get("/pictures") {
-                    status should equal(200)
-                    body should include("index")
-                }
-            }
-            "get pictures show" in {
-                get("/pictures/12") {
-                    status should equal(200)
-                    body should include("show 12")
-                }
-            }
-            "post pictures create" in {
-                post("/pictures") {
-                    status should equal(200)
-                    body should include("create")
-                }
-            }
-            "put pictures update" in {
-                put("/pictures/12") {
-                    status should equal(200)
-                    body should include("update")
-                }
-            }
-            "delete pictures destroy" in {
-                delete("/pictures/12") {
-                    status should equal(200)
-                    body should include("destroy")
-                }
-            }
+        get("/pictures") {
+          status should equal(400)
         }
+      }
+      "return a 500 code for unexpected exceptions" in {
+        scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
+          policies = Some(Map(
+            ~:[GoodPolicy] -> ~:[BadPolicyResponder],
+            ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
+          ),
+          service = Some(~:[ThrowsUnexpected]),
+          responder = Some(~:[PicturesIndexResponder]))
+        ))
+        scalatrafied.bootstrap()
 
-        "checking content type" should {
-            "return JSON by default" in {
-                get("/pictures") {
-                    header("Content-Type") should include("application/json")
-                }
-            }
-            "return overridden type" in {
-                scalatrafied.actions resource("pictures", index = Some(Action(Get, "index", ContentType.XML)))
-                scalatrafied.bootstrap()
-                get("/pictures") {
-                    header("Content-Type") should include("application/xml")
-                }
-            }
+        get("/pictures") {
+          status should equal(500)
         }
+      }
+      "return a 500 code for config error" in {
+        scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
+          policies = Some(Map(
+            ~:[GoodPolicy] -> ~:[BadPolicyResponder],
+            ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
+          ),
+          service = Some(~:[ThrowsConfig]),
+          responder = Some(~:[PicturesIndexResponder]))
+        ))
+        scalatrafied.bootstrap()
 
-        "doing exceptional cases" should {
-            "return a 400 code for bad request" in {
-                scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
-                    policies = Some(Map(
-                        ~:[GoodPolicy] -> ~:[BadPolicyResponder],
-                        ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
-                    ),
-                    service = Some(~:[ThrowsBadRequest]),
-                    responder = Some(~:[PicturesIndexResponder]))
-                ))
-                scalatrafied.bootstrap()
-
-                get("/pictures") {
-                    status should equal(400)
-                }
-            }
-            "return a 500 code for unexpected exceptions" in {
-                scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
-                    policies = Some(Map(
-                        ~:[GoodPolicy] -> ~:[BadPolicyResponder],
-                        ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
-                    ),
-                    service = Some(~:[ThrowsUnexpected]),
-                    responder = Some(~:[PicturesIndexResponder]))
-                ))
-                scalatrafied.bootstrap()
-
-                get("/pictures") {
-                    status should equal(500)
-                }
-            }
-            "return a 500 code for config error" in {
-                scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
-                    policies = Some(Map(
-                        ~:[GoodPolicy] -> ~:[BadPolicyResponder],
-                        ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
-                    ),
-                    service = Some(~:[ThrowsConfig]),
-                    responder = Some(~:[PicturesIndexResponder]))
-                ))
-                scalatrafied.bootstrap()
-
-                get("/pictures") {
-                    status should equal(500)
-                }
-            }
-            "return a custom code for custom exception" in {
-                scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
-                    policies = Some(Map(
-                        ~:[GoodPolicy] -> ~:[BadPolicyResponder],
-                        ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
-                    ),
-                    service = Some(~:[Throws403]),
-                    responder = Some(~:[PicturesIndexResponder]))
-                ))
-                scalatrafied.bootstrap()
-
-                get("/pictures") {
-                    status should equal(403)
-                }
-            }
+        get("/pictures") {
+          status should equal(500)
         }
+      }
+      "return a custom code for custom exception" in {
+        scalatrafied.actions resource("pictures", index = Some(Action(Get, "index",
+          policies = Some(Map(
+            ~:[GoodPolicy] -> ~:[BadPolicyResponder],
+            ~:[AuthenticationPolicy] -> ~:[BadPolicyResponder])
+          ),
+          service = Some(~:[Throws403]),
+          responder = Some(~:[PicturesIndexResponder]))
+        ))
+        scalatrafied.bootstrap()
 
-        "doing multiple format responses" should {
-            "work for JSON" in {
-                scalatrafied.actions action("multipleFormat", Get)
-                scalatrafied.bootstrap()
-
-                get("/multipleFormat", Nil, Map("Accept" -> JSON.toString)) {
-                    status should equal(200)
-                    header("Content-Type") should include(JSON.toString)
-                    body should be("some value")
-                }
-            }
-
-            "work for XML" in {
-                scalatrafied.actions action("multipleFormat", Get)
-                scalatrafied.bootstrap()
-
-                get("/multipleFormat", Nil, Map("Accept" -> XML.toString)) {
-                    status should equal(200)
-                    header("Content-Type") should include(XML.toString)
-                    body should be("some value")
-                }
-            }
-
-            "default to JSON when empty" in {
-                scalatrafied.actions action("multipleFormat", Get)
-                scalatrafied.bootstrap()
-
-                get("/multipleFormat") {
-                    status should equal(200)
-                    header("Content-Type") should include(JSON.toString)
-                    body should be("some value")
-                }
-            }
-
-            "work for a custom type" in {
-                scalatrafied.actions action("multipleFormat", Get)
-                scalatrafied.bootstrap()
-
-                get("/multipleFormat", Nil, Map("Accept" -> CSV.toString)) {
-                    status should equal(200)
-                    header("Content-Type") should include(CSV.toString)
-                    body should be("some value")
-                }
-            }
+        get("/pictures") {
+          status should equal(403)
         }
+      }
     }
+
+    "doing multiple format responses" should {
+      "work for JSON" in {
+        scalatrafied.actions action("multipleFormat", Get)
+        scalatrafied.bootstrap()
+
+        get("/multipleFormat", Nil, Map("Accept" -> JSON.toString)) {
+          status should equal(200)
+          header("Content-Type") should include(JSON.toString)
+          body should be("some value")
+        }
+      }
+
+      "work for XML" in {
+        scalatrafied.actions action("multipleFormat", Get)
+        scalatrafied.bootstrap()
+
+        get("/multipleFormat", Nil, Map("Accept" -> XML.toString)) {
+          status should equal(200)
+          header("Content-Type") should include(XML.toString)
+          body should be("some value")
+        }
+      }
+
+      "default to JSON when empty" in {
+        scalatrafied.actions action("multipleFormat", Get)
+        scalatrafied.bootstrap()
+
+        get("/multipleFormat") {
+          status should equal(200)
+          header("Content-Type") should include(JSON.toString)
+          body should be("some value")
+        }
+      }
+
+      "work for a custom type" in {
+        scalatrafied.actions action("multipleFormat", Get)
+        scalatrafied.bootstrap()
+
+        get("/multipleFormat", Nil, Map("Accept" -> CSV.toString)) {
+          status should equal(200)
+          header("Content-Type") should include(CSV.toString)
+          body should be("some value")
+        }
+      }
+    }
+  }
 }
